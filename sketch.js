836fb1d7e6ae55f84bf37ebc7a01cbb746ee5ca0
g-misheel <97901209+g-misheel@users.noreply.github.com>
2023-11-24 12:15:39 -0600

    added preload function for images

--- sketch.js
+++ sketch.js
@@ -5,40 +5,79 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
-let candies = ["blue", "green", "lightblue", "lightgreen", "orange", "pink", "red", "white", "yellow"];
-let board = [];
-let rows = 9;
-let columns = 9;
-let score = 0;
+let candies;
+let board;
+let rows;
+let columns;
+let score;
+let blueImg;
+let greenImg;
+let lightblueImg;
+let lightgreenImg;
+let orangeImg;
+let pinkImg;
+let redImg;
+let whiteImg;
+let yellowImg;
+
+
+function preload() {
+  blueImg = loadImage("images/blue.png");
+  greenImg = loadImage("images/green.png");
+  lightblueImg = loadImage("images/lightblue.png");
+  lightgreenImg = loadImage("images/lightgreen.png");
+  orangeImg = loadImage("images/orange.png");
+  pinkImg = loadImage("images/pink.png");
+  redImg = loadImage("images/red.png");
+  whiteImg = loadImage("images/white.png");
+  yellowImg = loadImage("images/yellow.png");
+}
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  candies = ["blue", "green", "lightblue", "lightgreen", "orange", "pink", "red", "white", "yellow"];
+  // candies = [blueImg, greenImg, lightblueImg, lightgreenImg, orangeImg, pinkImg, redImg, whiteImg, yellowImg];
+  board = [];
+  rows = 9;
+  columns = 9;
+  score = 0;
 }
 
 function draw() {
-  startGame();
+  // startGame();
 }
 
-function randomCandy() {
-  return candies[Math.floor(Math.random() * candies.length)];  //0 - 8.99
+class Candy {
+  constructor(x, y, type) {
+    this.x = x;
+    this.y = y;
+    this.type = type;
+  }
 }
 
-function startGame() {
-  for (let y = 0; y < rows; y++) {
-    let row = [];
-    for (let x = 0; x < columns; x++) {
-      // <img>
-      let tile = document.createElement("img");
-      tile.id = y.toString() + "-" + x.toString();
+
+
+// function randomCandy() {
+//   return candies[Math.floor(Math.random() * candies.length)];  //0 - 8.99
+// }
+
+// function startGame() {
+//   for (let y = 0; y < rows; y++) {
+//     let row = [];
+//     for (let x = 0; x < columns; x++) {
+      //  <img id="0-0" src="./images/red.png">
+
+      // let tile = document.createElement("img");
+      // tile.id = y.toString() + "-" + x.toString();
       //set the tile.src to a random candy
-      tile.src = "./images" + randomCandy() + ".png"; //the name of your image file
+      // tile.src = "./images" + randomCandy() + ".png"; //the name of your image file
 
 
       //11:25
-      document.getElementById("board").append(tile);
-      row.push(tile);
-    }
-    board.push(row);
-  }
-  console.log(board);
-}
+      // document.getElementById("board").append(tile);
+      // row.push(tile);
+//     }
+//     board.push(row);
+//   }
+//   // console.log(board);
+// }
